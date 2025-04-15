96794b8fce91775ed001a595d6c9cde570cdc062
DarkKiller28 <oyunbaskani7728@gmail.com>
2025-04-15 07:25:07 +0300

    script file editing

--- yaap_a15.sh
+++ yaap_a15.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/DarkKiller28/local_manifests_tulip .repo/local_manifests -b evo_a15/qpr2 
+git clone https://github.com/DarkKiller28/local_manifests_tulip .repo/local_manifests -b yaap_a15/qpr2 
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -20,15 +20,6 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-# Curl Signing Files
-git clone https://github.com/germaniumsculk/certs
-
-#SIGNING_KEY_PATH ?= certs
-#RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
-#SIGNING_KEY_PATH ?= certs
-#RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
-#PRODUCT_DEFAULT_DEV_CERTIFICATE := $(RELEASE_KEY)
-#PRODUCT_OTA_PUBLIC_KEYS := $(RELEASE_KEY)
 
 # Have to add these flags in device.mk
 
@@ -43,7 +34,7 @@ source build/envsetup.sh
 echo "============="
 
 # Lunch
-lunch lineage_bouquet-bp1a-userdebug
+lunch yaap_bouquet-bp1a-userdebug
 
 # Build
-m evolution
+m yaap
